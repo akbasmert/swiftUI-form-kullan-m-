5af73a96e97b814cb39f5812feac39f2d8ecd91e
mert <mert.ma346@gmail.com>
2022-10-12 15:25:00 +0300

    form kullanımını denedik.

--- form kullanimi/ContentView.swift
+++ form kullanimi/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var hatirlatmaAcikMi = false
+    
+    @State private var zamanListesi = ["1 Gün","1 Hafta","1 Ay"]
+    @State private var secilmisZamanIndeks = 0
+    
+    @State private var odemeListesi = ["Haftalık","Aylık","Yıllık"]
+    @State private var secilenOdemeIndeks = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                
+                Section(header: Text("Hatırlatma"), footer:Text("Bildirim sıklığını ayarlayabilirsiniz")){
+                    
+                    Picker(selection: $secilmisZamanIndeks, label: Text("")){
+                        ForEach(0..<self.zamanListesi.count){
+                            Text(self.zamanListesi[$0]).tag($0)
+                        }
+                        
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    
+                    Toggle(isOn:$hatirlatmaAcikMi){
+                        Text("Hatırlatma Kapat")
+                    }
+                }
+                
+                Section(footer:Text("Ödeme sıklığını ayarlayabilirsiniz")){
+                    
+                    Picker(selection: $secilenOdemeIndeks, label: Text("Ödeme Sıklığı")){
+                        ForEach(0..<self.odemeListesi.count){
+                            Text(self.odemeListesi[$0]).tag($0)
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+                Section{
+                    
+                    Button(action:{
+                        
+                        print("Hatırlatma : \(self.zamanListesi[self.secilmisZamanIndeks])")
+                        
+                        print("Hatırlatma  Açık Mı : \(self.hatirlatmaAcikMi)")
+                        
+                        print("Ödeme Sıklığı : \(self.odemeListesi[self.secilenOdemeIndeks])")
+                        
+                        
+                        
+                    }){
+                        Text("Değişiklikleri Kaydet")
+                    }
+                    
+                    
+                }
+                
+                
+            }.navigationBarTitle("Ayarlar")
         }
-        .padding()
     }
 }
 
