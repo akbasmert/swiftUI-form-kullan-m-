a00dca4d9703e2e0977572a90297bf9e895ffb99
mert <mert.ma346@gmail.com>
2022-10-12 15:22:42 +0300

    Initial Commit

---
+++ form kullanimi/form_kullanimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  form_kullanimiApp.swift
+//  form kullanimi
+//
+//  Created by Mert AKBAŞ on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct form_kullanimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
